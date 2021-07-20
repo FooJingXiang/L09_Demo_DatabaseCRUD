5767ade7a3feb57831559cb4b2dc5d13f23caa3e
FOO JING XIANG <20011262@myrp.edu.sg>
2021-07-20 21:40:41 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20011262/demodatabasecrud/MainActivity.java
@@ -0,0 +1,103 @@
+package sg.edu.rp.c346.id20011262.demodatabasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd, btnEdit, btnRetrieve;
+    EditText et;
+    ArrayList<Note> al;
+    ListView lv;
+    ArrayAdapter<Note> aa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd=findViewById(R.id.buttonEdit);
+        btnEdit=findViewById(R.id.buttonEdit);
+        btnRetrieve=findViewById(R.id.buttonRetrieve);
+        et=findViewById(R.id.editTextContent);
+        lv=findViewById(R.id.listview);
+
+        al = new ArrayList<>();
+        aa = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, al);
+        lv.setAdapter(aa);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = et.getText().toString();
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long inserted_id = dbh.insertNote(data);
+
+                if (inserted_id != -1){
+                    al.clear();
+                    al.addAll(dbh.getAllNotes());
+                    aa.notifyDataSetChanged();
+                    Toast.makeText(MainActivity.this, "Insert successful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                al.clear();
+                //al.addAll(dbh.getAllNotes());
+                String filterText = et.getText().toString().trim();
+                if(filterText.length() == 0) {
+                    al.addAll(dbh.getAllNotes());
+                }
+                else{
+                    al.addAll(dbh.getAllNotes(filterText));
+                }
+
+                aa.notifyDataSetChanged();
+            }
+        });
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Note target = al.get(0);
+
+                Intent i = new Intent(MainActivity.this,
+                        EditActivity.class);
+                i.putExtra("data", target);
+                startActivity(i);
+            }
+        });
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Note target = al.get(position);
+                Intent i = new Intent(MainActivity.this, EditActivity.class);
+                i.putExtra("data", target);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        btnRetrieve.performClick();
+    }
+}
